cb4a63237491710033c542fb9d265da1a06143d8
Ours Brindille <ours.brindille@gmail.com>
2020-03-12 12:49:31 +0100

    Mobile UI - ongoing

--- frontend/src/components/mobilePageIndex.jsx
+++ frontend/src/components/mobilePageIndex.jsx
@@ -9,12 +9,16 @@ import 'react-tabs/style/react-tabs.css';
 mapboxgl.accessToken = 'pk.eyJ1Ijoic2Fib3N0aXgiLCJhIjoiY2p6Y3BkdnJ4MDd2czNjbWdsYXB4MTJoNSJ9.U65hBBejoDAAJH5wrdLejg';
 
 var count = 0
-var baseURL = process.env.BACKEND_SERVER_URL
+var baseURL = process.env.REACT_APP_BACKEND_SERVER_URL
 
 export function gethttp(){
   var http = require('superagent');
   return http;
 }
+
+console.log("toto")
+
+console.log(baseURL)
 
 class MobilePageIndex extends Component { 
     
